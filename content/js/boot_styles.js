308fe367a952c323d8cbe8facbce3b4066f55aa7
mxc <matt@curinga.com>
2024-01-30 11:56:10 -0500

    updated js so it created ids for headers

--- content/js/boot_styles.js
+++ content/js/boot_styles.js
@@ -1,9 +1,29 @@
 
+function safeName(name) {
+    // replace non-alphanumeric characters with underscores
+    let safe = name.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+    // replace multiple underscores with a single one
+    safe = safe.replace(/_+/g, '_');
+    // remove leading and trailing underscores
+    
+    let i = 0;
+    while (document.getElementById(safe)) {
+        safe = safe + i;
+        i++;
+    }
+    return safe;
+}
+
 function addBootstrapStyles() {
     const headers = document.getElementsByTagName("h2");
     const addLink = (header)=> {
 
         let link = document.createElement("a");
+        let id = header.id;
+        if (!id || id === "") {
+            id = safeName(header.textContent);
+            header.id = id;
+        }
         link.href = "#" + header.id ;
 
         let icon = document.createElement("i");
